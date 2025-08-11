6c35620a0dec4a7f4424dad50ec34575504cb620
jujak1105-crypto <jujak1105@gmail.com>
2025-08-11 09:43:58 +0900

    ex.폴더 이전 후 새로 올림

--- src/data/anime.js
+++ src/data/anime.js
@@ -1,3 +1,227 @@
-export const AnimeText = [
+import anime01 from "../assets/img/anime/anime01.jpg"  
+import anime02 from "../assets/img/anime/anime02.jpg"  
+import anime03 from "../assets/img/anime/anime03.jpg"  
+import anime04 from "../assets/img/anime/anime04.jpg"  
+import anime05 from "../assets/img/anime/anime05.jpg"  
+import anime06 from "../assets/img/anime/anime06.jpg"  
+import anime07 from "../assets/img/anime/anime07.jpg"  
+import anime08 from "../assets/img/anime/anime08.jpg"  
+import anime09 from "../assets/img/anime/anime09.jpg"  
+import anime10 from "../assets/img/anime/anime10.jpg"  
+import anime11 from "../assets/img/anime/anime11.jpg"  
+import anime12 from "../assets/img/anime/anime12.jpg"  
+import anime13 from "../assets/img/anime/anime13.jpg"  
+import anime14 from "../assets/img/anime/anime14.jpg"  
+import anime15 from "../assets/img/anime/anime15.jpg"  
+import anime16 from "../assets/img/anime/anime16.jpg"  
+import anime17 from "../assets/img/anime/anime17.jpg"  
+import anime18 from "../assets/img/anime/anime18.jpg"  
+import anime19 from "../assets/img/anime/anime19.jpg"  
+import anime20 from "../assets/img/anime/anime20.jpg"  
+import anime21 from "../assets/img/anime/anime21.jpg"  
+import anime22 from "../assets/img/anime/anime22.jpg"  
+import anime23 from "../assets/img/anime/anime23.jpg"  
+import anime24 from "../assets/img/anime/anime24.jpg"  
+import anime25 from "../assets/img/anime/anime25.jpg"  
+import anime26 from "../assets/img/anime/anime26.jpg"  
+import anime27 from "../assets/img/anime/anime27.jpg"  
+import anime28 from "../assets/img/anime/anime28.jpg"  
 
-]
+export const AnimeText =[ 
+
+    {   title: "86 -에이티식스-",
+        img: anime01,
+        author: "백곰선배",
+        videoId: "Cxkjo_u3kyM",
+        channelId: "UCF2NEA2Wkhcc4vHchkXGeWQ"
+    },
+    {
+        title: "이별의 아침에 약속의 꽃을 장식하자",
+        img: anime02,
+        author: "애푸덕",
+        videoId: "LNazY4nTCdo",
+        channelId: "UCHRATg5P9sNQcM6MxZoYegQ"
+    },
+    {
+        title: "타코피의 원죄",
+        img: anime03,
+        author: "백곰선배",
+        videoId: "wnvoDYShEe4",
+        channelId: "UCF2NEA2Wkhcc4vHchkXGeWQ"
+    },
+    {
+        title: "메달리스트",
+        img: anime04,
+        author: "나롱이네",
+        videoId: "1MHnrhBPfZQ",
+        channelId: "UCdxuGQUbk-SeJlZxMtLGzAg"
+    },
+    {
+        title:"거미입니다만 문제라도 2기",
+        img:anime05,
+        author:"@ohlekun",
+        videoId:"twgPQ6DU4ck",
+        channelId:"UCHmcxdt1ce3EGEYtrxE2-cw"
+    },
+    {
+        title:"지박소년 하나코 군",
+        img:anime06,
+        author:"@ANIPLUS_kr",
+        videoId:"Weqbp79KpuU",
+        channelId:"UCqu9dHrj8jqbF3HP5BM31LQ"
+    },
+    {
+        title:"어둠의 실력자가 되고싶어서 2기",
+        img:anime07,
+        author:"@이세계애니리뷰",
+        videoId:"4mQsVbsT6G0",
+        channelId:"UCu-4IVbFUBygAVnl2xNHpEQ"
+    },
+    {
+        title:"달이 이끄는 이세계 여행",
+        img:anime08,
+        author:"@ANIPLUSINC",
+        video:"AqQXdIXJXXw",
+        channelId:"UCciFL1bEM7JxsFX4W2HzqnA"
+    },
+    {
+        title:"이세계 방랑 밥",
+        img:anime09,
+        author:"AniplusTV",
+        videoid:"ENEcTcCG43c",
+        channelid:"UCciFL1bEM7JxsFX4W2HzqnA"
+    },
+    {
+        title:"이세계 식당",
+        img:anime10,
+        author:"애니팝",
+        videoid:"XhYwe2VMN-0",
+        channelid:"UCyEVLsni8NgFGKwW7FSEXRQ"
+    },
+    {
+        title:"비와 너와",
+        img:anime11,
+        author:"애푸덕",
+        videoid:"UFOkLHKMlPA",
+        channelid:"UCHRATg5P9sNQcM6MxZoYegQ"
+    },
+    {
+        title:"미이라 사육법",
+        img:anime12,
+        author:"Marcus Mapua",
+        videoid:"GyH7xTfMLCs",
+        channelid:"UCms247CtD6fmLoxjbULbpiw"
+    },
+    {
+        title:"아이스에이지",
+        img:anime13,
+        author:"애니집합소",
+        videoId:"56zURpR_gBk",
+        datachannelId:"UCHtqZO9YM7EtpuKhNKVWHDQ"
+    },
+    {
+        title:"위대한 사기꾼",
+        img:anime14,
+        author:"애니집합소",
+        videoId:"nQ6qb-nG0Sc",
+        datachannelId:"UCHtqZO9YM7EtpuKhNKVWHDQ"
+    },
+    {
+        title:"약속의 네버랜드",
+        img:anime15,
+        author:"애니 다이스",
+        videoId:"mMiW6Mox-Ys",
+        datachannelId:"UCKHKAueK_-X-5lJGpC1dtWg"
+    },
+    {
+        title:"신을 죽였다",
+        img:anime16,
+        author:"애죽애살",
+        videoId:"56zURpR_gBk",
+        datachannelId:"UCnM5Daq9-_8tVmPmUra3Tww"
+    },
+    {
+        title:"O MENINO E O MUNDO",
+        img:anime17,
+        author:"Cinédito",
+        videoId:"Fzej2MTKgts",
+        channelId:"UChwHGHEVoIpWhWaE_oYyhgw"
+    },
+    {
+        title:"The Red Turtle",
+        img:anime18,
+        author:"Zero Media",
+        videoId:"Sw7BggqBpTk",
+        channelId:"UCuuU2L90sqhQahV1IkhCPaQ"
+    },
+    {
+        title:"felix and the hidden treasure trailer",
+        img:anime19,
+        author:"JoBlo Animated Videos",
+        videoId:"13QbJDsffw0",
+        channelId:"UC2sxxXRBL5SgY0fI58BrOCA"
+    },
+    {
+        title:"The Breadwinner",
+        img:anime20,
+        author:"RGS WALY ",
+        videoId:"FgQK0Z14zmU",
+        channelId:"UCB1o7bFWk3V0f6ynQ3BPymQ"
+    },
+    {
+        title: "녹을 먹는 비스코",
+        img: anime21,
+        author: "도토리군",
+        videoId: "n7Z8j4hNNLo",
+        channelId: "UCG5KF8qimTCNUCB0GQvFWag"
+    },
+    {
+        title: "아마도 역사상 가장 위대한 연금술사",
+        img: anime22,
+        author: "하루한편애니",
+        videoId: "IrwvtWkbWk0",
+        channelId: "UCiyBXLUYItVS85iLd1n7Njw"
+    },
+    {
+        title: "이세계 느긋한 기행",
+        img: anime23,
+        author: "덕애니",
+        videoId: "7ubWOa0_KWE",
+        channelId: "UCJ8SkP--q1RLO3OmpBXE6Xw"
+    },
+    {
+        title: "메르크 스토리아",
+        img: anime24,
+        author: "오덕냥",
+        videoId: "nABt1B6rCfI",
+        channelId: "UC552lhAIfvgPvnSc4xhYirw"
+    },
+    {
+        title: "나루토",
+        img: anime25,
+        author: "깡스 애니",
+        videoId: "VOLPXuRmVO4",
+        channelId: "UCKuUYspmBI6-dNB1YebhD3w"
+    },
+    {
+        title: "카드캡터체리",
+        img: anime26,
+        author: "알잘딱깔센",
+        videoId: "K2zKJuCOWe8",
+        channelId: "UCYFzDHtLDmRJd2gUiVd_hDw"
+    },
+    {
+        title:"바람이 강하게 불고 있다",
+        img:anime27,
+        author:"ANIPLUS",
+        videoId:"1vAQWa8-GSs",
+        channelId:"UCqu9dHrj8jqbF3HP5BM31LQ"
+    },
+    {
+        title:"드래곤볼",
+        img:anime28,
+        author:"단풍손",
+        videoId:"wPR_aGNycww",
+        channelId:"UCrf19pIQQsjRNeaixO1ETTg"
+    },
+    ]
